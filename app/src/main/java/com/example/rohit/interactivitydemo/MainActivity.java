ca9998b3997bb0edadef80f44de1afd24450e16b
Rohit <rohitdr786@gmail.com>
2018-11-22 01:33:04 -0500

    First Commit

---
+++ app/src/main/java/com/example/rohit/interactivitydemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.rohit.interactivitydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void printLog(View view){
+        Log.i("PrintButtonLog","Button Clicked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
